c0aa24a02deae5de27ab2271ad82317bd4fa5164
ice6504 <aphinan6504@gmail.com>
2024-02-10 13:38:04 +0700

    API Example

---
+++ API_test/axios.js
@@ -0,0 +1,21 @@
+let info = document.getElementById("tbody");
+const url =
+  "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+
+axios.get(url).then((response) => {
+  console.log(response.data[0].image_link);
+  response.data.map((element) => {
+    const { id, name, price, description, image_link } = element;
+    let newRow = `
+        <tr>
+        <th>
+            <h1>${id}</h1>
+        </th>
+        <td class="flex flex-col gap-5 text-center"><img src="${image_link}">${name}</td>
+        <td class="text-center" >$ ${price}</td>
+        <td>${description}</td>
+        </tr>
+        `;
+    info.innerHTML += newRow;
+  });
+});
---
+++ API_test/fetch.js
@@ -0,0 +1,25 @@
+async function loadData() {
+    const url =
+      "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+    const response = await fetch(url);
+    const data = await response.json();
+    let info = document.getElementById("tbody");
+  
+    data.map((element) => {
+      const { name, description, image_link } = element;
+      let newRow = `
+          <tr>
+          <th>
+            <label>
+              <input id="check" type="checkbox" class="checkbox" />
+            </label>
+          </th>
+          <td class="flex flex-col gap-5 text-center"><img src="${image_link}">${name}</td>
+          <td>${description}</td>
+          </tr>
+          `;
+      console.log(newRow);
+      info.innerHTML += newRow;
+    });
+  }
+  
